ed957b375eb0009a8780ae65ff36c80138c71856
Diego Giraldo <dgiraldogaleano@gmail.com>
2023-07-24 19:57:30 -0500

    saber el ultimo elemento de un array de una manera mas facil y sencilla

---
+++ es13/00-at.js
@@ -0,0 +1,5 @@
+const array = ["one", "two", "three", "five", "six"]
+
+console.log(array[array.length - 1]);
+
+console.log(array.at(- 1)) // saber el ultimo elemento de un array de una manera mas facil y sencilla
